ea9b1454eba43befc629923c26f0f6623bd62cbc
Rebekah Becky <39873704+Abbaskhurram255@users.noreply.github.com>
2023-07-08 10:34:42 +0500

    Update script.js

--- music/script.js
+++ music/script.js
@@ -1,6 +1,6 @@
 
 document.body.onload = () => {
-	window.setTimeout(() => { document.querySelector("#audio1").play(); }, 1500);
+	window.setTimeout(() => { document.querySelector("#audio1").play(); }, 2000);
 };
 
 $(() => {
