74af94e27b40ad31d0504df33790c3007c8b2aa7
Rebekah Becky <39873704+Abbaskhurram255@users.noreply.github.com>
2023-07-08 21:28:04 +0500

    Add files via upload

--- dictionary/app.js
+++ dictionary/app.js
@@ -1,37 +1,66 @@
-const url = "https://api.dictionaryapi.dev/api/v2/entries/en/"
+const url = "https://api.dictionaryapi.dev/api/v2/entries/en/";
 
-const result = document.getElementById('result')
-const sound = document.getElementById('sound')
-const btn = document.getElementById('search-btn')
+const result = document.getElementById('result');
+const sound = document.getElementById('sound');
+const btn = document.getElementById('search-btn');
+window.$ = function(selector) {
+  return document.querySelector(selector);
+}
+
+$("input").addEventListener("keydown", function (e) {
+    const code = e.keyCode ? e.keyCode : e.which;
+    if (code == 13) {
+      btn.click();
+      }
+});
+
+const isOdd = number => {
+	let res = number % 2 != 0;
+	console.log(res);
+	return res;
+}
 
 btn.addEventListener('click', () => {
-    let inpWord = document.getElementById('inp-word').value
+    let inpWord = document.getElementById('inp-word').value;
     fetch(`${url}${inpWord}`)
-        .then((response) => response.json())
-        .then((data) => {
-            console.log(data)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data[0].meanings[0].definitions.length);
             result.innerHTML = `
             <div class="word">
             <h3>${inpWord}</h3>
-            <button onclick="playSound()"><img src="volume-high-solid.png" alt="volume"
+            <button onclick="playSound()"><img src="volume-high-solid.png" alt="pronunciation"
                 class="volume" ></button>
             </div>
                 <div class="details">
                     <p>${data[0].meanings[0].partOfSpeech}</p>
                     <p>${data[0].phonetic}</p>
                 </div>
-                <p class="word-meaning">
-                    ${data[0].meanings[0].definitions[0].definition}
-                </p>
+                <p class="word-meaning">`;
+                /*for (let x = 0; x < data[0].meanings.length; x++) {*/
+                	for (let y = 0; y < data[0].meanings[0].definitions.length; y++) {
+                	result.innerHTML += `${data[0].meanings[0].definitions[y].definition}<br />`;
+                if (isOdd(y)) result.innerHTML += '<br />';
+                /*}*/
+                }
+                result.innerHTML += `</p>
                 <p class="word-example">
                     ${data[0].meanings[0].definitions[0].
                     example || ""}
-                </p>`
-            sound.setAttribute("src", `${data[0].
-            phonetics[0].audio}`)
+                </p>
+                 <p class="word-example">`;
+                 for (let num = 0; num < data[0].meanings[0].synonyms.length; ++num) {
+                 	result.innerHTML += `<strong style="font-size:20px;color:green">:</strong> ${data[0].meanings[0].synonyms[num] || ""}<br />`;
+                 }
+                    result.innerHTML += 
+                `</p>`;
+            sound.src = data[0].phonetics[0].audio;
         })
-        .catch( () => {
-            result.innerHTML = `<h3 class= "error">Couldn't Find The Word<h3>`
+        .catch(e => {
+        	console.log("Error: " + e);
+        if (/undefined/i.test(e)) {
+            result.innerHTML = "<h3 class= 'error'>Couldn't Find That Word, <span id='blink'>Sorry!</span><h3>";
+            }
         })
     })
 
