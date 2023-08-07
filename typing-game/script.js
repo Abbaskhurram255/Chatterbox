ca0c5e1046c86d0ae85878fe7b9feec0c6379a6c
Rebekah Becky aka Ayesha Mehnaaz <39873704+Abbaskhurram255@users.noreply.github.com>
2023-08-07 23:35:43 +0500

    Add files via upload

--- typing-game/script.js
+++ typing-game/script.js
@@ -1,3 +1,37 @@
+// Speech
+const utterance = new SpeechSynthesisUtterance();
+utterance.addEventListener("boundary", (e) => {
+  currentCharacter = e.charIndex;
+});
+
+function playText(text, pitch = 1.0) {
+  if (speechSynthesis.paused && speechSynthesis.speaking) {
+    return speechSynthesis.resume();
+  }
+  if (speechSynthesis.speaking) return;
+  utterance.text = text;
+  var voices = window.speechSynthesis.getVoices();
+  window.speechSynthesis.onvoiceschanged = function () {
+    voices = window.speechSynthesis.getVoices();
+  };
+  utterance.voice = voices[10];
+  utterance.pitch = pitch;
+  utterance.voiceURI = "native";
+  utterance.lang = "hi";
+  utterance.volume = 1;
+  utterance.rate = 1;
+  speechSynthesis.speak(utterance);
+}
+
+//Call this function to (immediately) stop the Speech synthesis:
+function stopText() {
+  speechSynthesis.resume();
+  speechSynthesis.cancel();
+}
+//end of Speech_Synth block
+
+
+
 const word = document.getElementById('word');
 const text = document.getElementById('text');
 const scoreEl = document.getElementById('score');
@@ -11,25 +45,426 @@ const difficultySelect = document.getElementById('difficulty');
 // List of words for game
 const words = [
   'sigh',
-  'tense',
-  'airplane',
-  'ball',
-  'pies',
-  'juice',
-  'warlike',
+  'intense',
+  'pregnancy',
+  'child labor',
+  'period',
+  'menstruation',
+  'breast augmentation',
+  'lip fillers',
+  'lipstick',
+  'earrings',
+  'jewellery',
+  'hearrings',
+  'eye liner',
+  'lip liner',
   'bad',
-  'north',
+  'no way',
   'dependent',
-  'steer',
+  'steering wheel',
+  'goldenrod',
   'silver',
+  'purple',
+  'orchid',
+  'hotpink',
+  'deeppink',
+  'yellowish',
+  'orangish',
+  'peachy',
+  'liable',
+  'liability',
+  'reliance',
+  'lovable',
+  'adorable',
+  'cute',
+  'charming',
+  'highly attractive',
+  'hot',
+  'horny',
+  'adultery',
+  'vaginal',
+  'transparency',
+  'translucent',
+  'hallucinations',
+  'combination',
+  'illusion',
   'highfalutin',
   'superficial',
-  'quince',
-  'eight',
+  'laalchi',
+  'princess',
+  'queen',
+  'duchess',
+  'prince',
+  'king',
+  'Unites States',
+  'Romania',
+  'French',
+  'German',
+  'Russian',
+  'riksha',
+  'rakshaa bandhan',
+  'eid',
+  'oops, sorry',
+  'bait',
+  'shikaar',
   'feeble',
   'admit',
-  'drag',
-  'loving'
+  'orgasm',
+  'loving',
+  'high as fuck',
+  'drugged',
+  'fuzzy',
+  'dizzy',
+  'biker',
+  'wheels',
+  'generator',
+  'preoccupied',
+  'enormous',
+  'supervision',
+  'revision',
+  'allowances',
+  'royal',
+  'regal',
+  'heavenly',
+  'hindi',
+  'artificial intelligence',
+  'fragment',
+  'pigmentation',
+  'decoloration',
+  'panties',
+  'party balloons',
+  'blouses',
+  'makeup',
+  'dress',
+  'lehenga',
+  'boyfriend',
+  'dopamine',
+  'serotonin', 
+  'neurochemistry',
+  'love chemistry',
+  'oxytocin',
+  'foundation',
+  'false lashes',
+  'eyeshadow palette',
+  'mascara',
+  'blond hair',
+  'brunette girl',
+  'wig',
+  'clitoris',
+  'clitoral stimulation',
+  'vibrator',
+  'BDSM',
+  'sadism',
+  'sadomasochistic',
+  'vaginal penetration',
+  'sugar daddy',
+  'LGBTQIA2S+',
+  'queer',
+  'bisexual',
+  'asexual',
+  'aromantic',
+  'lesbian',
+  'demiboy',
+  'demigirl',
+  'bigender',
+  'engineer',
+  'trampoline',
+  'lily',
+  'dandelion',
+  'freeware',
+  'millan',
+  'husband & wife',
+  'tea',
+  'ransomware',
+  'malware',
+  'spyware',
+  'trojan horse',
+  'maleficent',
+  'malfunctional',
+  'encode',
+  'decoder',
+  'compiler',
+  'decompiler',
+  'mehnat',
+  'maafi',
+  'ummeed',
+  'aasra',
+  'bharwasaa',
+  'pyaar ka izhaar',
+  'bamboo',
+  'baansuri',
+  'flute',
+  'piano',
+  'synthesizer',
+  'guitar',
+  'sugarcoating',
+  'ocean waves ',
+  'sea breeze',
+  'swimming',
+  'surfing',
+  'roller skates',
+  'rollercoaster',
+  'Selena Gomez',
+  'Love You Like A Love Song',
+  'Taylor Swift',
+  'Ellie Goulding',
+  'Ryan Gosling',
+  'Calvin Harris',
+  'gorgeous',
+  'elegant',
+  'handsome',
+  'fascinating',
+  'cute',
+  'kind',
+  'giving',
+  'caring',
+  'loving',
+  'fun to talk to',
+  'brilliance',
+  'fabulous',
+  'fictious',
+  'pretentious',
+  'pretending',
+  'mimicry',
+  'fiction',
+  'sci-fi',
+  'romance',
+  'rom-com',
+  'marvelous',
+  'comical',
+  'fancy',
+  'fantasy',
+  'dreamy',
+  'magnificent',
+  'breathtakin\'',
+  'rockstar',
+  'teenager',
+  'sugarcane',
+  'shanaakht',
+  'pehchaan',
+  'mangaytar',
+  'dushman',
+  'ehsaas',
+  'shaadi',
+  'nikaah',
+  'jurm',
+  'assault',
+  'murder',
+  'merderous',
+  'sexual assault',
+  'marital rape',
+  'rape',
+  'akaylapan',
+  'mehek',
+  'gurdbaar',
+  'zehmat',
+  'taskeen',
+  'adlo insaaf',
+  'tashreef',
+  'tasleem karna',
+  'fiance',
+  'fiancee',
+  'insaaf',
+  'equality',
+  'herat angez',
+  'secrecy',
+  'mammary glands',
+  'sweat glands',
+  'biochemistry',
+  'hormones',
+  'estrogen',
+  'antiandrogens',
+  'therapist',
+  'vaginoplasty',
+  'sex reassignment',
+  'music production',
+  'indoctrination',
+  'coronavirus',
+  'vaccination',
+  'vulva',
+  'urinary tract',
+  'telescope',
+  'microscopic',
+  'acceptance',
+  'denial',
+  'aggression',
+  'regret',
+  'museebat',
+  'arrogance',
+  'obligance',
+  'zimma',
+  'zimmedaari',
+  'pashtaawa',
+  'duplication',
+  'magic spell',
+  'resume',
+  'circulation',
+  'respiratory',
+  'vandalism',
+  'havoc',
+  'chaos',
+  'mishap',
+  'tabaahi',
+  'nooksaan',
+  'prevalence',
+  'disaster',
+  'deconstruction',
+  'loss',
+  'death',
+  'penalty',
+  'jail cell',
+  'girl in a coma',
+  'khidmat',
+  'hospitality',
+  'crimson',
+  'magenta',
+  'darkred',
+  'darkgreen',
+  'cyan',
+  'lightgreen',
+  'seagreen',
+  'tutti fruiti',
+  'assessibility',
+  'ease of access',
+  'cease to exist',
+  'decibels',
+  'mazbooti',
+  'frequency',
+  'rid oneself of',
+  'good riddance!',
+  'prince of darkness',
+  'firefly',
+  'android',
+  'programming language',
+  'hormonal therapy',
+  'jhoota',
+  'shikaayat',
+  'chaahat',
+  'muhabbat',
+  'chehraa',
+  'ghoonghat',
+  'chiknaahat',
+  'begunaah',
+  'typewriter',
+  'proofreading',
+  'job description',
+  'devour',
+  'decoy',
+  'decorations',
+  'contrast',
+  'interactivity',
+  'idle',
+  'HIV',
+  'STD',
+  'liberation',
+  'sublimation',
+  'intuition',
+  'counterintuitive',
+  'claustrophobia',
+  'sharmo hayaa',
+  'social anxiety',
+  'shyness',
+  'feminism',
+  'ibuprofen',
+  'anesthesia',
+  'methamphetamine',
+  'Adderall',
+  'painkillers',
+  'SSRIs',
+  'SNRIs',
+  'antidepressants',
+  'ICU nurse',
+  'x-ray',
+  'ultrasound',
+  'contract',
+  'counteract',
+  'north west',
+  'New Nork',
+  'Colorado',
+  'resignation',
+  'expiration',
+  'degradation',
+  'little by little',
+  'kisi ko ghataana',
+  'gradually',
+  'tragic',
+  'khatraa',
+  'khatarnaak',
+  'kahaawat',
+  'akwaal',
+  'chaal',
+  'maal',
+  'ghar',
+  'khayaal',
+  'karsaaz',
+  'majburi',
+  'khidmaat',
+  'khayalaat',
+  'tasawwar',
+  'wujoohaat',
+  'hevaaniyaat',
+  'masroofiyaat',
+  'manzooriyaat',
+  'dead end',
+  'deadline',
+  'bardaasht',
+  'deep enough',
+  'deepening',
+  'penetration',
+  'distraction',
+  'mind blowing',
+  'hooker',
+  'slut',
+  'cunt',
+  'jugnu',
+  'cunning',
+  'beautiful',
+  'stunning',
+  'mahaarat',
+  'kamzori',
+  'shehnaaz',
+  'aqsa',
+  'hafsa',
+  'iqra',
+  'classroom',
+  'angels\' lullaby',
+  'hajaab',
+  'citrus',
+  'sour',
+  'delicious',
+  'lemonade',
+  'luminance',
+  'teekhapan',
+  'karwaa',
+  'meetha',
+  'sinister',
+  'transition',
+  'OCD',
+  'ADHD',
+  'nonbinary',
+  'MTF',
+  'FTM',
+  'SRS',
+  'meeting',
+  'applesauce',
+  'orange juice',
+  'soap',
+  'detergent',
+  'cutlery',
+  'crockery',
+  'timeline',
+  'kitchen',
+  'rubik\'s cube',
+  'khof',
+  'dardnaak',
+  'dharkan',
+  'heartbeat',
+  'internet trends',
+  'meme',
+  'queue',
+  'lovebirds',
+  'love life',
+  'Ayushman Khurrana',
 ];
 
 // Init word
@@ -59,7 +494,7 @@ text.focus();
 // Start counting down
 const timeInterval = setInterval(updateTime, 1000);
 
-// Generate random word from array
+// Generate random words from the array containing all the words
 function getRandomWord() {
   return words[Math.floor(Math.random() * words.length)];
 }
@@ -68,6 +503,7 @@ function getRandomWord() {
 function addWordToDOM() {
   randomWord = getRandomWord();
   word.innerHTML = randomWord;
+  playText(randomWord);
 }
 
 // Update score
@@ -81,7 +517,7 @@ function updateTime() {
   time--;
   timeEl.innerHTML = time + 's';
 
-  if (time === 0) {
+  if (time == 0) {
     clearInterval(timeInterval);
     // end game
     gameOver();
@@ -91,10 +527,13 @@ function updateTime() {
 // Game over, show end screen
 function gameOver() {
   endgameEl.innerHTML = `
-    <h1>Time ran out</h1>
-    <p>Your final score is ${score}</p>
-    <button onclick="location.reload()">Reload</button>
+    <h1>You're Out of Time!</h1>
+    <p>Your final score is ${score}<br/><span style='color:orange'>Better luck next time!</span></p>
+    <button onclick="stopText();location.href='?stupid_user=true';">Reload</button>
   `;
+  let comments = ["bas yehi speed he kya? Oho!", "jab ek game ko handle nahi kar paa rahay, tau life ko kese handle karte hoge?", "ab iss se slow bhi koi likhta he kyaa? huh-huh!", "Mujhe ni lagtaa iss say slow bhi koi likhtaa hogaa. Dubaara try karne ki himmat hey to kijye zaraa. Huh-huh!"];
+  stopText();
+  playText(comments[Math.floor(Math.random() * comments.length)]);
 
   endgameEl.style.display = 'flex';
 }
@@ -107,19 +546,19 @@ addWordToDOM();
 text.addEventListener('input', e => {
   const insertedText = e.target.value;
 
-  if (insertedText === randomWord) {
+  if (insertedText.toLowerCase() == randomWord.toLowerCase()) {
     addWordToDOM();
     updateScore();
 
     // Clear
     e.target.value = '';
 
-    if (difficulty === 'hard') {
+    if (difficulty == 'hard') {
       time += 2;
-    } else if (difficulty === 'medium') {
+    } else if (difficulty == 'medium') {
       time += 3;
     } else {
-      time += 5;
+      time += 5; //if it's the easy mode
     }
 
     updateTime();
