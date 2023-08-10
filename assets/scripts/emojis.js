946a031be27f5cdc53fab22e6ab2f3ff28530a84
Rebekah Becky aka Ayesha Mehnaaz <39873704+Abbaskhurram255@users.noreply.github.com>
2023-08-10 07:44:19 +0500

    Add files via upload

--- assets/scripts/emojis.js
+++ assets/scripts/emojis.js
@@ -1,188 +1,188 @@
 //let cry 2 stay "cry", and cry "cry2", if you what I mean --- DO NOT EDIT!
 
 let sad7, sad8, disappointed, disappointed2, disappointed3, disappointed4, disappointed5, disappointed6, disappointed7, disappointed8, whine, whiny, complain, complaining, complainer, ugh, disgust, disgusting;
-const sad = disappointed = " <img class='emoji' src='assets/images/emo/sad.png' />";
-const sad2 = disappointed2 = " <img class='emoji' src='assets/images/emo/sad2.png' />";
-const sad3 = disappointed3 = " <img class='emoji' src='assets/images/emo/sad3.png' />";
-const sad4 = disappointed4 = " <img class='emoji' src='assets/images/emo/sad4.png' />";
-const sad5 = disappointed5 = " <img class='emoji' src='assets/images/emo/sad5.png' />";
-const sad6 = disappointed6 = " <img class='emoji' src='assets/images/emo/sad6.png' />";
+const sad = disappointed = " <img alt='emoji' class='emoji' src='assets/images/emo/sad.png' />";
+const sad2 = disappointed2 = " <img alt='emoji' class='emoji' src='assets/images/emo/sad2.png' />";
+const sad3 = disappointed3 = " <img alt='emoji' class='emoji' src='assets/images/emo/sad3.png' />";
+const sad4 = disappointed4 = " <img alt='emoji' class='emoji' src='assets/images/emo/sad4.png' />";
+const sad5 = disappointed5 = " <img alt='emoji' class='emoji' src='assets/images/emo/sad5.png' />";
+const sad6 = disappointed6 = " <img alt='emoji' class='emoji' src='assets/images/emo/sad6.png' />";
 
-const worried = sad7 = disappointed7 = " <img class='emoji' src='assets/images/emo/worried.png' />";
+const worried = sad7 = disappointed7 = " <img alt='emoji' class='emoji' src='assets/images/emo/worried.png' />";
 
-const pessimistic = sad8 = disappointed8 = whine = whiny = complain = complaining = complainer = ugh = disgust = disgusting = " <img class='emoji' src='assets/images/emo/pessimistic.png' />";
+const pessimistic = sad8 = disappointed8 = whine = whiny = complain = complaining = complainer = ugh = disgust = disgusting = " <img alt='emoji' class='emoji' src='assets/images/emo/pessimistic.png' />";
 
 
-const cry = " <img class='emoji' src='assets/images/emo/cry2.png' />";
-const cry2 = " <img class='emoji' src='assets/images/emo/cry.png' />";
+const cry = " <img alt='emoji' class='emoji' src='assets/images/emo/cry2.png' />";
+const cry2 = " <img alt='emoji' class='emoji' src='assets/images/emo/cry.png' />";
 
-const sob = " <img class='emoji' src='assets/images/emo/sob1.png' />";
-const sob2 = " <img class='emoji' src='assets/images/emo/sob2.png' />";
+const sob = " <img alt='emoji' class='emoji' src='assets/images/emo/sob1.png' />";
+const sob2 = " <img alt='emoji' class='emoji' src='assets/images/emo/sob2.png' />";
 
 
 let smile, smiles, phew, phew2, wonderful, lol3, in_mood;
-const happy = smile = smiles = " <img class='emoji' src='assets/images/emo/happy.png' />";
-const relieved = phew = " <img class='emoji' src='assets/images/emo/relieved.png' />";
-const calm = " <img class='emoji' src='assets/images/emo/calm.png' />";
-const tears_of_joy = phew2 = wonderful = " <img class='emoji' src='assets/images/emo/tears_of_joy.png' />";
-const rolling_out_of_joy = lol3 = " <img class='emoji' src='assets/images/emo/rolling_out_of_joy.png' />";
-const flip = in_mood = " <img class='emoji' src='assets/images/emo/flip.png' />";
+const happy = smile = smiles = " <img alt='emoji' class='emoji' src='assets/images/emo/happy.png' />";
+const relieved = phew = " <img alt='emoji' class='emoji' src='assets/images/emo/relieved.png' />";
+const calm = " <img alt='emoji' class='emoji' src='assets/images/emo/calm.png' />";
+const tears_of_joy = phew2 = wonderful = " <img alt='emoji' class='emoji' src='assets/images/emo/tears_of_joy.png' />";
+const rolling_out_of_joy = lol3 = " <img alt='emoji' class='emoji' src='assets/images/emo/rolling_out_of_joy.png' />";
+const flip = in_mood = " <img alt='emoji' class='emoji' src='assets/images/emo/flip.png' />";
 
 
 let ego, glasses, hero, heroine, smirk, smirkface, flirt, flirtface, flirting, flirty, seduce, seduction, is_that_so; 
-const attitude = ego = hero = heroine = glasses = " <img class='emoji' src='assets/images/emo/attitude.png' />";
-const as_if = smirk = smirkface = seduce = seduction = flirt = flirtface = flirting = flirty = is_that_so = " <img class='emoji' src='assets/images/emo/as_if.png' />";
+const attitude = ego = hero = heroine = glasses = " <img alt='emoji' class='emoji' src='assets/images/emo/attitude.png' />";
+const as_if = smirk = smirkface = seduce = seduction = flirt = flirtface = flirting = flirty = is_that_so = " <img alt='emoji' class='emoji' src='assets/images/emo/as_if.png' />";
 
 
-const awesome = " <img class='emoji' src='assets/images/emo/awesome.png' />";
-const awesome2 = " <img class='emoji' src='assets/images/emo/awesome2.png' />";
+const awesome = " <img alt='emoji' class='emoji' src='assets/images/emo/awesome.png' />";
+const awesome2 = " <img alt='emoji' class='emoji' src='assets/images/emo/awesome2.png' />";
 
-const amazed = " <img class='emoji' src='assets/images/emo/amazed.png' />";
+const amazed = " <img alt='emoji' class='emoji' src='assets/images/emo/amazed.png' />";
 
 
-const wow = " <img class='emoji' src='assets/images/emo/wow.png' />";
-const whoa2 = " <img class='emoji' src='assets/images/emo/whoa.png' />";
+const wow = " <img alt='emoji' class='emoji' src='assets/images/emo/wow.png' />";
+const whoa2 = " <img alt='emoji' class='emoji' src='assets/images/emo/whoa.png' />";
 
 let shy, heart, love, inLove, in_Love;
-const blush = shy = " <img class='emoji' src='assets/images/emo/blush.png' />";
-const hearts = heart = love = " <img class='emoji' src='assets/images/emo/hearts.png' />";
-const aww = " <img class='emoji' src='assets/images/emo/aww.png' />";
-const in_love = inLove = in_Love = " <img class='emoji' src='assets/images/emo/in_love.png' />";
+const blush = shy = " <img alt='emoji' class='emoji' src='assets/images/emo/blush.png' />";
+const hearts = heart = love = " <img alt='emoji' class='emoji' src='assets/images/emo/hearts.png' />";
+const aww = " <img alt='emoji' class='emoji' src='assets/images/emo/aww.png' />";
+const in_love = inLove = in_Love = " <img alt='emoji' class='emoji' src='assets/images/emo/in_love.png' />";
 
 
-const satisfied = " <img class='emoji' src='assets/images/emo/satisfied.png' />";
-const satisfied2 = " <img class='emoji' src='assets/images/emo/satisfied2.png' />";
-const satisfied3 = " <img class='emoji' src='assets/images/emo/satisfied3.png' />";
+const satisfied = " <img alt='emoji' class='emoji' src='assets/images/emo/satisfied.png' />";
+const satisfied2 = " <img alt='emoji' class='emoji' src='assets/images/emo/satisfied2.png' />";
+const satisfied3 = " <img alt='emoji' class='emoji' src='assets/images/emo/satisfied3.png' />";
 
 
-const duh = " <img class='emoji' src='assets/images/emo/duh.png' />";
+const duh = " <img alt='emoji' class='emoji' src='assets/images/emo/duh.png' />";
 
 let confused, doubt, confused2, confused3, whoa, unbelievable;
-const what = confused = " <img class='emoji' src='assets/images/emo/what.png' />";
-const what2 = confused2 = whoa = unbelievable = " <img class='emoji' src='assets/images/emo/what2.png' />";
-const what3 = confused3 = " <img class='emoji' src='assets/images/emo/what3.png' />";
-const wtf = doubt = what4 = confused4 = " <img class='emoji' src='assets/images/emo/wtf.png' />";
+const what = confused = " <img alt='emoji' class='emoji' src='assets/images/emo/what.png' />";
+const what2 = confused2 = whoa = unbelievable = " <img alt='emoji' class='emoji' src='assets/images/emo/what2.png' />";
+const what3 = confused3 = " <img alt='emoji' class='emoji' src='assets/images/emo/what3.png' />";
+const wtf = doubt = what4 = confused4 = " <img alt='emoji' class='emoji' src='assets/images/emo/wtf.png' />";
 
 
 let sleeping, exhausted, exhausted1, exhausted2, exhausted3, exhausted4;
-const tired = exhausted = exhausted1 = " <img class='emoji' src='assets/images/emo/tired.png' />";
-const tired2 = exhausted2 = " <img class='emoji' src='assets/images/emo/tired2.png' />";
-const tired3 = exhausted3 = " <img class='emoji' src='assets/images/emo/tired3.png' />";
+const tired = exhausted = exhausted1 = " <img alt='emoji' class='emoji' src='assets/images/emo/tired.png' />";
+const tired2 = exhausted2 = " <img alt='emoji' class='emoji' src='assets/images/emo/tired2.png' />";
+const tired3 = exhausted3 = " <img alt='emoji' class='emoji' src='assets/images/emo/tired3.png' />";
 
-const zzz = sleeping = exhausted4 = " <img class='emoji' src='assets/images/emo/zzz.png' />";
-
-
-const hungry = " <img class='emoji' src='assets/images/emo/hungry.png' />";
-const thirsty = " <img class='emoji' src='assets/images/emo/thirsty.png' />";
+const zzz = sleeping = exhausted4 = " <img alt='emoji' class='emoji' src='assets/images/emo/zzz.png' />";
 
 
-const kiss = " <img class='emoji' src='assets/images/emo/kiss.png' />";
-const kiss2 = " <img class='emoji' src='assets/images/emo/kiss2.png' />";
+const hungry = " <img alt='emoji' class='emoji' src='assets/images/emo/hungry.png' />";
+const thirsty = " <img alt='emoji' class='emoji' src='assets/images/emo/thirsty.png' />";
 
 
-const haha = " <img class='emoji' src='assets/images/emo/haha.png' />";
-const hehe = " <img class='emoji' src='assets/images/emo/hehe.png' />";
+const kiss = " <img alt='emoji' class='emoji' src='assets/images/emo/kiss.png' />";
+const kiss2 = " <img alt='emoji' class='emoji' src='assets/images/emo/kiss2.png' />";
 
-const lol = " <img class='emoji' src='assets/images/emo/lol.png' />";
-const lol2 = " <img class='emoji' src='assets/images/emo/lol2.png' />";
+
+const haha = " <img alt='emoji' class='emoji' src='assets/images/emo/haha.png' />";
+const hehe = " <img alt='emoji' class='emoji' src='assets/images/emo/hehe.png' />";
+
+const lol = " <img alt='emoji' class='emoji' src='assets/images/emo/lol.png' />";
+const lol2 = " <img alt='emoji' class='emoji' src='assets/images/emo/lol2.png' />";
 
 
 let shit, bullshit, trash, dead, clown, sick, swear, ew, ew2, eww, eww2;
-const ghost = " <img class='emoji' src='assets/images/emo/ghost.png' />";
-const joker = clown = " <img class='emoji' src='assets/images/emo/joker.png' />";
-const bruh = " <img class='emoji' src='assets/images/emo/swag.png' />";
-const crap = shit = trash = bullshit = " <img class='emoji' src='assets/images/emo/crap.png' />";
-const censored = swear = " <img class='emoji' src='assets/images/emo/censored.png' />";
-const skull = dead = " <img class='emoji' src='assets/images/emo/dead.png' />";
-const dead2 = " <img class='emoji' src='assets/images/emo/dead2.png' />";
-const vomit = ew = eww = " <img class='emoji' src='assets/images/emo/nausea.png' />";
-const puke = ew2 = eww2 = " <img class='emoji' src='assets/images/emo/puke.png' />";
-const mask = sick = " <img class='emoji' src='assets/images/emo/mask.png' />";
-const sick2 = " <img class='emoji' src='assets/images/emo/sick.png' />";
+const ghost = " <img alt='emoji' class='emoji' src='assets/images/emo/ghost.png' />";
+const joker = clown = " <img alt='emoji' class='emoji' src='assets/images/emo/joker.png' />";
+const bruh = " <img alt='emoji' class='emoji' src='assets/images/emo/swag.png' />";
+const crap = shit = trash = bullshit = " <img alt='emoji' class='emoji' src='assets/images/emo/crap.png' />";
+const censored = swear = " <img alt='emoji' class='emoji' src='assets/images/emo/censored.png' />";
+const skull = dead = " <img alt='emoji' class='emoji' src='assets/images/emo/dead.png' />";
+const dead2 = " <img alt='emoji' class='emoji' src='assets/images/emo/dead2.png' />";
+const vomit = ew = eww = " <img alt='emoji' class='emoji' src='assets/images/emo/nausea.png' />";
+const puke = ew2 = eww2 = " <img alt='emoji' class='emoji' src='assets/images/emo/puke.png' />";
+const mask = sick = " <img alt='emoji' class='emoji' src='assets/images/emo/mask.png' />";
+const sick2 = " <img alt='emoji' class='emoji' src='assets/images/emo/sick.png' />";
 
 
 let scream_louder, screamLoud;
-const scream = " <img class='emoji' src='assets/images/emo/scream.png' />";
-const scream2 = scream_louder = screamLoud = " <img class='emoji' src='assets/images/emo/scream_louder.png' />";
+const scream = " <img alt='emoji' class='emoji' src='assets/images/emo/scream.png' />";
+const scream2 = scream_louder = screamLoud = " <img alt='emoji' class='emoji' src='assets/images/emo/scream_louder.png' />";
 
-const scared = " <img class='emoji' src='assets/images/emo/scared.png' />";
+const scared = " <img alt='emoji' class='emoji' src='assets/images/emo/scared.png' />";
 
 
-const shh = " <img class='emoji' src='assets/images/emo/shh.png' />";
-const silence = " <img class='emoji' src='assets/images/emo/sillence.png' />";
-const secrecy = " <img class='emoji' src='assets/images/emo/secrecy.png' />";
-const speechless = " <img class='emoji' src='assets/images/emo/speechless.png' />";
+const shh = " <img alt='emoji' class='emoji' src='assets/images/emo/shh.png' />";
+const silence = " <img alt='emoji' class='emoji' src='assets/images/emo/sillence.png' />";
+const secrecy = " <img alt='emoji' class='emoji' src='assets/images/emo/secrecy.png' />";
+const speechless = " <img alt='emoji' class='emoji' src='assets/images/emo/speechless.png' />";
 
 
 let winkface, noway, lying;
-const wink = winkface = " <img class='emoji' src='assets/images/emo/wink.png' />";
-const wink2 = " <img class='emoji' src='assets/images/emo/wink2.png' />";
-const wink3 = " <img class='emoji' src='assets/images/emo/wink3.png' />";
-const wink4 = " <img class='emoji' src='assets/images/emo/wink4.png' />";
+const wink = winkface = " <img alt='emoji' class='emoji' src='assets/images/emo/wink.png' />";
+const wink2 = " <img alt='emoji' class='emoji' src='assets/images/emo/wink2.png' />";
+const wink3 = " <img alt='emoji' class='emoji' src='assets/images/emo/wink3.png' />";
+const wink4 = " <img alt='emoji' class='emoji' src='assets/images/emo/wink4.png' />";
 
-const no_way = noway = " <img class='emoji' src='assets/images/emo/no_way.png' />";
-const liar = lying = " <img class='emoji' src='assets/images/emo/liar.png' />";
+const no_way = noway = " <img alt='emoji' class='emoji' src='assets/images/emo/no_way.png' />";
+const liar = lying = " <img alt='emoji' class='emoji' src='assets/images/emo/liar.png' />";
 
 
 let robot, robot2, robot3, robo, robo2, robo3, AI, AI2, AI3;
-const ai = AI = robot = robo = " <img class='emoji' src='assets/images/emo/machine.png' />";
-const ai2 = AI2 = robot2 = robo2 = " <img class='emoji' src='assets/images/emo/robot.png' />";
-const ai3 = AI3 = robot3 = robo3 = " <img class='emoji' src='assets/images/emo/robo.png' />";
+const ai = AI = robot = robo = " <img alt='emoji' class='emoji' src='assets/images/emo/machine.png' />";
+const ai2 = AI2 = robot2 = robo2 = " <img alt='emoji' class='emoji' src='assets/images/emo/robot.png' />";
+const ai3 = AI3 = robot3 = robo3 = " <img alt='emoji' class='emoji' src='assets/images/emo/robo.png' />";
 
 
 let you_cant;
-const fight = you_cant = " <img class='emoji' src='assets/images/emo/fight.png' />";
+const fight = you_cant = " <img alt='emoji' class='emoji' src='assets/images/emo/fight.png' />";
 
 
 let money, cash;
-const dollar = money = cash = " <img class='emoji' src='assets/images/emo/cash.png' />";
+const dollar = money = cash = " <img alt='emoji' class='emoji' src='assets/images/emo/cash.png' />";
 
 
 let singing, hum;
-const sing = singing = hum = " <img class='emoji' src='assets/images/emo/sing.png' />";
+const sing = singing = hum = " <img alt='emoji' class='emoji' src='assets/images/emo/sing.png' />";
 
 
 let sweetDreams, sweet_dreams, sweet_dream,goodnight;
-const gnight = goodnight = sweetDreams = sweet_dreams = sweet_dream = " <img class='emoji' src='assets/images/emo/gnight.png' />";
+const gnight = goodnight = sweetDreams = sweet_dreams = sweet_dream = " <img alt='emoji' class='emoji' src='assets/images/emo/gnight.png' />";
 
 
 let thinking;
-const think = thinking = " <img class='emoji' src='assets/images/emo/think.png' />";
+const think = thinking = " <img alt='emoji' class='emoji' src='assets/images/emo/think.png' />";
 
 
 let ribbon;
-const winner = ribbon = " <img class='emoji' src='assets/images/emo/ribbon.png' />";
+const winner = ribbon = " <img alt='emoji' class='emoji' src='assets/images/emo/ribbon.png' />";
 
 
 let cmon, comeon;
-const come_on = cmon = comeon = " <img class='emoji' src='assets/images/emo/come_on.png' />";
+const come_on = cmon = comeon = " <img alt='emoji' class='emoji' src='assets/images/emo/come_on.png' />";
 
 
 let godly, god, mercy, merciful, angel, angelica, angela, heavenly, haylou, halo, princess;
-const goddess = godly = god = mercy = merciful = angel = angela = angelica = heavenly = haylou = halo = princess = " <img class='emoji' src='assets/images/emo/goddess.png' />";
+const goddess = godly = god = mercy = merciful = angel = angela = angelica = heavenly = haylou = halo = princess = " <img alt='emoji' class='emoji' src='assets/images/emo/goddess.png' />";
 
 
 let got_it, got_It, gotit, gotIt;
-const detective = got_it = got_It = gotit = gotIt = " <img class='emoji' src='assets/images/emo/detective.png' />";
+const detective = got_it = got_It = gotit = gotIt = " <img alt='emoji' class='emoji' src='assets/images/emo/detective.png' />";
 
 
-const glowing_teeth = " <img class='emoji' src='assets/images/emo/glowing_teeth.png' />";
+const glowing_teeth = " <img alt='emoji' class='emoji' src='assets/images/emo/glowing_teeth.png' />";
 
 
 let frustrated, frustrated2, frustration, frustration2, mad, mad2, madness, madness2, rage, rage2, outrage, outrage2, outrageous, outrageous2, anger, anger2, wrath, wrath2;
-const angry = frustrated = frustration = mad = madness = rage = outrage = outrageous = anger = wrath = " <img class='emoji' src='assets/images/emo/angry1-32.png' />";
-const angry2 = frustrated = frustrated2 = frustration = frustration2 = mad2 = madness2 = rage2 = outrage2 = outrageous2 = anger2 = wrath2 = " <img class='emoji' src='assets/images/emo/angry2-32.png' />";
+const angry = frustrated = frustration = mad = madness = rage = outrage = outrageous = anger = wrath = " <img alt='emoji' class='emoji' src='assets/images/emo/angry1-32.png' />";
+const angry2 = frustrated = frustrated2 = frustration = frustration2 = mad2 = madness2 = rage2 = outrage2 = outrageous2 = anger2 = wrath2 = " <img alt='emoji' class='emoji' src='assets/images/emo/angry2-32.png' />";
 
 
 let not_funny, boring, booring;
-const meh = not_funny = boring = booring = " <img class='emoji' src='assets/images/emo/you_sure.png' />";
+const meh = not_funny = boring = booring = " <img alt='emoji' class='emoji' src='assets/images/emo/you_sure.png' />";
 
 
 let bandage, that_hurt, that_hurts, hurt, pain, in_pain;
-const ouch = bandage = that_hurt = that_hurts = pain = in_pain = " <img class='emoji' src='assets/images/emo/bandage.png' />";
+const ouch = bandage = that_hurt = that_hurts = pain = in_pain = " <img alt='emoji' class='emoji' src='assets/images/emo/bandage.png' />";
 
 let apology, apologize, uhoh, oops, oopsie, oopsy;
-const sorry = apology = apologize = uhoh = oops = oopsie = oopsy = " <img class='emoji' src='assets/images/emo/sorry.png' />";
+const sorry = apology = apologize = uhoh = oops = oopsie = oopsy = " <img alt='emoji' class='emoji' src='assets/images/emo/sorry.png' />";
 
 
 
