27ad20ccf6949997378abdf96d66edbee28d5b43
Ayesha Mehnaaz <39873704+Abbaskhurram255@users.noreply.github.com>
2024-12-29 13:52:21 +0500

    .

--- dress up/main.js
+++ dress up/main.js
@@ -12,23 +12,6 @@ nextface();
 //function to change dress
 function nextdress() {
   let dress = document.querySelector("#dress");
-  //code from the tutorial example:
-  //   if (state.dress === 0) {
-  //     dress.setAttribute("class", "dress1");
-  //     state.dress++;
-  //     console.log(state);
-  //   } else if (state.dress === 1) {
-  //     dress.setAttribute("class", "dress2");
-  //     state.dress++;
-  //     console.log(state);
-  //   } else if (state.dress === 2) {
-  //     dress.setAttribute("class", "dress3");
-  //     //set back to 0
-  //     state.dress = 0;
-  //     console.log(state);
-  //   }
-
-  //my refactored version with LESS code and MORE scalability:
   if (state.dress < 7) {
     state.dress++;
     dress.setAttribute("class", `dress${state.dress}`);
